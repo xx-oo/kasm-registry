9cba771e2dd4abd38aa762a0bb1c218ff6924453
a <a@pkxu.com>
2025-08-17 15:12:42 +0800

    更新

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'cnm Technologies',
+    description: '搬运原版镜像回国加速',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://registry.cnm.cool/',
+    contactUrl: 'https://cnm.cool/support',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
